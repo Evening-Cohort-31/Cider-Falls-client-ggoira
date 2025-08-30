c48113f6e39e22ee320b07bcd05a07a71b854ce2
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-08-29 20:45:52 -0500

    Added junction database entry for destination services

--- scripts/database.js
+++ scripts/database.js
@@ -73,7 +73,7 @@ const database = {
             name: "zip lines"
         }
     ], 
-    guests :[
+    guests : [
         { id: 1, first_name: "Olivia", last_name: "Smith" },
         { id: 2, first_name: "Liam", last_name: "Johnson" },
         { id: 3, first_name: "Emma", last_name: "Williams" },
@@ -89,6 +89,37 @@ const database = {
         { id: 13, first_name: "Benjamin", last_name: "Wilson" },
         { id: 14, first_name: "Amelia", last_name: "Anderson" },
         { id: 15, first_name: "Elijah", last_name: "Thomas" }
+    ], 
+    destination_services : [
+        // Chamfort River (id: 1): rafting, canoeing, fishing
+        { destination_id: 1, service_id: 1 }, // rafting
+        { destination_id: 1, service_id: 2 }, // canoeing
+        { destination_id: 1, service_id: 3 }, // fishing
+
+        // Lost Wolf Hiking Trail (id: 2): hiking, picnicking, rock climbing
+        { destination_id: 2, service_id: 4 }, // hiking
+        { destination_id: 2, service_id: 5 }, // picnicking
+        { destination_id: 2, service_id: 6 }, // rock climbing
+
+        // Lodge (id: 3): lodging, parking, information, picnicking
+        { destination_id: 3, service_id: 7 }, // lodging
+        { destination_id: 3, service_id: 8 }, // parking
+        { destination_id: 3, service_id: 9 }, // information
+        { destination_id: 3, service_id: 5 }, // picnicking
+
+        // Gander River (id: 4): fishing, hiking
+        { destination_id: 4, service_id: 3 }, // fishing
+        { destination_id: 4, service_id: 4 }, // hiking
+
+        // Campgrounds (id: 5): information, lodging, parking
+        { destination_id: 5, service_id: 9 }, // information
+        { destination_id: 5, service_id: 7 }, // lodging
+        { destination_id: 5, service_id: 8 }, // parking
+
+        // Pine Bluff Trails (id: 6): hiking, picnicking, zip lines
+        { destination_id: 6, service_id: 4 }, // hiking
+        { destination_id: 6, service_id: 5 }, // picnicking
+        { destination_id: 6, service_id: 10 } // zip lines
     ]
 }
 
@@ -102,4 +133,8 @@ export const getServices = () => {
 
 export const getGuests = () => {
     return structuredClone(database.guests)
+}
+
+export const getDestinationServices = () => {
+    return structuredClone(database.destination_services)
 }
